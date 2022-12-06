a2e360a015dd13ac9eda89f2abe9692708f253c0
Timmer, Daan <daan.timmer@philips.com>
2022-12-06 13:26:46 +0100

    Story #88276: Cleanup of TracingGapPeripheralSt tracing messages
    (cherry picked from commit 4ffcb7256b509d9d3d77ce3f1fcc1e8e4b1a5325)

--- hal_st/middlewares/ble_middleware/TracingGapPeripheralSt.cpp
+++ hal_st/middlewares/ble_middleware/TracingGapPeripheralSt.cpp
@@ -9,81 +9,80 @@ namespace hal
 
     void TracingGapPeripheralSt::HandleHciDisconnectEvent(hci_event_pckt& eventPacket)
     {
-        auto disconnectEvt = reinterpret_cast<hci_disconnection_complete_event_rp0*>(eventPacket.data);
-
-        tracer.Trace() << "Disconnect for connection handle: " << disconnectEvt->Connection_Handle;
+        const auto disconnectEvt = reinterpret_cast<hci_disconnection_complete_event_rp0*>(eventPacket.data);
+        tracer.Trace() << "GapPeripheralSt::HandleHciDisconnectEvent handle = " << disconnectEvt->Connection_Handle;
         GapPeripheralSt::HandleHciDisconnectEvent(eventPacket);
     }
 
     void TracingGapPeripheralSt::HandleHciUnknownEvent(hci_event_pckt& eventPacket)
     {
-        tracer.Trace() << "HandleHciUnknownEvent: Unsupported event (" << eventPacket.evt << ")";
+        tracer.Trace() << "GapPeripheralSt::HandleHciUnknownEvent " << eventPacket.evt;
         GapPeripheralSt::HandleHciUnknownEvent(eventPacket);
     }
 
     void TracingGapPeripheralSt::HandleHciLeConnectionUpdateEvent(evt_le_meta_event* metaEvent)
     {
-        auto evtConnectionUpdate = reinterpret_cast<hci_le_connection_update_complete_event_rp0*>(metaEvent->data);
-        tracer.Trace() << "Connection update event";
-        tracer.Trace() << "Connection Interval  : " << evtConnectionUpdate->Conn_Interval;
-        tracer.Trace() << "Connection Latency   : " << evtConnectionUpdate->Conn_Latency;
-        tracer.Trace() << "Supervision Timeout   : " << evtConnectionUpdate->Supervision_Timeout;
+        const auto evtConnectionUpdate = reinterpret_cast<hci_le_connection_update_complete_event_rp0*>(metaEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandleHciLeConnectionUpdateEvent";
+        tracer.Trace() << "\tConnection Interval : " << evtConnectionUpdate->Conn_Interval;
+        tracer.Trace() << "\tConnection Latency  : " << evtConnectionUpdate->Conn_Latency;
+        tracer.Trace() << "\tSupervision Timeout : " << evtConnectionUpdate->Supervision_Timeout;
         GapPeripheralSt::HandleHciLeConnectionUpdateEvent(metaEvent);
     }
 
     void TracingGapPeripheralSt::HandleHciLePhyUpdateCompleteEvent(evt_le_meta_event* metaEvent)
     {
-        auto evtLePhyUpdate = reinterpret_cast<hci_le_phy_update_complete_event_rp0*>(metaEvent->data);
-        tracer.Trace() << "Phy update complete event, status " << evtLePhyUpdate->Status;
+        const auto evtLePhyUpdate = reinterpret_cast<hci_le_phy_update_complete_event_rp0*>(metaEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandleHciLePhyUpdateCompleteEvent " << evtLePhyUpdate->Status;
         GapPeripheralSt::HandleHciLePhyUpdateCompleteEvent(metaEvent);
     }
 
     void TracingGapPeripheralSt::HandleHciLeConnectionCompleteEvent(evt_le_meta_event* metaEvent)
     {
-        auto connectionCompleteEvt = reinterpret_cast<hci_le_connection_complete_event_rp0*>(metaEvent->data);
-        tracer.Trace() << "Connection complete event : Handle - " << connectionCompleteEvt->Connection_Handle;
+        const auto connectionCompleteEvt = reinterpret_cast<hci_le_connection_complete_event_rp0*>(metaEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandleHciLeConnectionCompleteEvent Handle - " << connectionCompleteEvt->Connection_Handle;
 
         hal::MacAddress mac;
         std::reverse_copy(std::begin(connectionCompleteEvt->Peer_Address),
-                std::end(connectionCompleteEvt->Peer_Address), std::begin(mac));
+            std::end(connectionCompleteEvt->Peer_Address), std::begin(mac));
         tracer.Continue() << " Peer address - " << infra::AsMacAddress(mac);
         GapPeripheralSt::HandleHciLeConnectionCompleteEvent(metaEvent);
     }
 
     void TracingGapPeripheralSt::HandleHciLeDataLengthUpdateEvent(evt_le_meta_event* metaEvent)
     {
-        auto dataLengthUpdate = reinterpret_cast<hci_le_data_length_change_event_rp0*>(metaEvent->data);
-        tracer.Trace() << "Data length change event";
-        tracer.Trace() << "MaxTxOctets  : " << dataLengthUpdate->MaxTxOctets;
-        tracer.Trace() << "MaxTxTime    : " << dataLengthUpdate->MaxTxTime;
-        tracer.Trace() << "MaxRxOctets  : " << dataLengthUpdate->MaxRxOctets;
-        tracer.Trace() << "MaxRxTime    : " << dataLengthUpdate->MaxRxTime;
+        const auto dataLengthUpdate = reinterpret_cast<hci_le_data_length_change_event_rp0*>(metaEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandleHciLeDataLengthUpdateEvent";
+        tracer.Trace() << "\tMaxTxOctets : " << dataLengthUpdate->MaxTxOctets;
+        tracer.Trace() << "\tMaxTxTime   : " << dataLengthUpdate->MaxTxTime;
+        tracer.Trace() << "\tMaxRxOctets : " << dataLengthUpdate->MaxRxOctets;
+        tracer.Trace() << "\tMaxRxTime   : " << dataLengthUpdate->MaxRxTime;
         GapPeripheralSt::HandleHciLeDataLengthUpdateEvent(metaEvent);
     }
 
     void TracingGapPeripheralSt::HandleHciLeUnknownEvent(evt_le_meta_event* metaEvent)
     {
-        tracer.Trace() << "HandleHciLeUnknownEvent : Unsupported event (" << metaEvent->subevent << ")";
+        tracer.Trace() << "GapPeripheralSt::HandleHciLeUnknownEvent " << metaEvent->subevent;
         GapPeripheralSt::HandleHciLeUnknownEvent(metaEvent);
     }
 
     void TracingGapPeripheralSt::HandleBondLostEvent(evt_blecore_aci* vendorEvent)
     {
-        tracer.Trace() << "Bond lost event: Send allow rebond";
+        tracer.Trace() << "GapPeripheralSt::HandleBondLostEvent";
         GapPeripheralSt::HandleBondLostEvent(vendorEvent);
     }
 
     void TracingGapPeripheralSt::HandlePairingCompleteEvent(evt_blecore_aci* vendorEvent)
     {
-        auto pairingComplete = reinterpret_cast<aci_gap_pairing_complete_event_rp0*>(vendorEvent->data);
-        tracer.Trace() << "GAP pairing complete : Status = " << pairingComplete->Status;
+        const auto pairingComplete = reinterpret_cast<aci_gap_pairing_complete_event_rp0*>(vendorEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandlePairingCompleteEvent " << pairingComplete->Status;
         GapPeripheralSt::HandlePairingCompleteEvent(vendorEvent);
     }
 
     void TracingGapPeripheralSt::HandleMtuExchangeEvent(evt_blecore_aci* vendorEvent)
     {
-        auto mtuExchangeEvent = reinterpret_cast<aci_att_exchange_mtu_resp_event_rp0*>(vendorEvent->data);
-        tracer.Trace() << "MTU Exhange event : Server_RX_MTU = " << mtuExchangeEvent->Server_RX_MTU;
+        const auto mtuExchangeEvent = reinterpret_cast<aci_att_exchange_mtu_resp_event_rp0*>(vendorEvent->data);
+        tracer.Trace() << "GapPeripheralSt::HandleMtuExchangeEvent Server_RX_MTU = " << mtuExchangeEvent->Server_RX_MTU;
         GapPeripheralSt::HandleMtuExchangeEvent(vendorEvent);
     }
 }
