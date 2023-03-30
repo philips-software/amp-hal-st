b06e5597d983eb0c943b88fcf798da4fdc947db3
Thomas de Bont <thomas.de.bont@philips.com>
2023-03-30 10:00:28 +0000

    Refactor and use InputPin/OutputPin

--- integration_test/stim/Main.cpp
+++ integration_test/stim/Main.cpp
@@ -9,12 +9,42 @@ extern "C" void Default_Handler()
     hal::InterruptTable::Instance().Invoke(hal::ActiveInterrupt());
 }
 
+class StimGpio {
+public:
+    explicit StimGpio(hal::GpioPin& inPin, hal::GpioPin& outPin, hal::GpioPin& outNPin);
+
+private:
+    void Update();
+
+    hal::InputPin in;
+    hal::OutputPin out;
+    services::GpioPinInverted outNPinInverted;
+    hal::OutputPin outN;
+};
+
+StimGpio::StimGpio(hal::GpioPin& inPin, hal::GpioPin& outPin, hal::GpioPin& outNPin)
+: in(inPin)
+, out(outPin)
+, outNPinInverted(outNPin)
+, outN(outNPinInverted)
+{
+    Update();
+    
+    in.EnableInterrupt([this]() { Update(); }, hal::InterruptTrigger::bothEdges);
+}
+
+void StimGpio::Update()
+{
+    const auto value = in.Get();
+    out.Set(value);
+    outN.Set(value);
+}
+
 int main()
 {
-    static hal::GpioPinStm nBooted(hal::Port::E, 3);
-    static services::GpioPinInverted booted(nBooted);
-
-    booted.Set(false);
+    static hal::GpioPinStm nBootedPin(hal::Port::E, 3);
+    static services::GpioPinInverted bootedPin(nBootedPin);
+    static hal::OutputPin booted(bootedPin, false);
 
     static main_::StmEventInfrastructure eventInfrastructure;
     static main_::NucleoF767ziUi ui;
@@ -23,17 +53,10 @@ int main()
     static hal::GpioPinStm inPin(hal::Port::F, 7);
     static hal::GpioPinStm outPin(hal::Port::F, 8);
     static hal::GpioPinStm outNPin(hal::Port::F, 9);
-    inPin.Config(hal::PinConfigType::input);
-    outNPin.Set(true);
-    
-    inPin.EnableInterrupt([]() {
-        const auto value = inPin.Get();
-        outPin.Set(value);
-        outNPin.Set(!value);
-    }, hal::InterruptTrigger::bothEdges);
+
+    static StimGpio stimGpio(inPin, outPin, outNPin);
 
     booted.Set(true);
-    
     eventInfrastructure.Run();
     __builtin_unreachable();
 }
--- integration_test/tester/Main.cpp
+++ integration_test/tester/Main.cpp
@@ -6,6 +6,7 @@
 #include "hal_st/stm32fxxx/DmaStm.hpp"
 #include "hal_st/stm32fxxx/UartStmDma.hpp"
 #include "services/util/GpioPinInverted.hpp"
+#include "services/util/DebouncedButton.hpp"
 
 /* Tester    Stim   Function
  * ------    ------ --------
@@ -23,69 +24,152 @@ extern "C" void Default_Handler()
     hal::InterruptTable::Instance().Invoke(hal::ActiveInterrupt());
 }
 
+class Report
+{
+public:
+    Report(hal::GpioPin& redPin, hal::GpioPin& greenPin)
+    : red(redPin)
+    , green(greenPin)
+    {}
+
+    enum class Condition
+    {
+        Ready,
+        Success,
+        Timeout,
+        FailedPrecondition,
+        Failed
+    };
+
+    void State(Condition condition)
+    {
+        red.Set(Conditions[static_cast<int>(condition)].red);
+        green.Set(Conditions[static_cast<int>(condition)].green);
+    }
+
+private:
+    hal::OutputPin red;
+    hal::OutputPin green;
+
+    struct
+    {
+        bool red;
+        bool green;
+    } Conditions[5] =
+    {
+        {true, true},
+        {false, true},
+        {true, false},
+        {true, false},
+        {true, false}
+    };
+};
+
+class Tester
+{
+public:
+    Tester(hal::GpioPin& resetPin, hal::GpioPin& bootedPin, hal::GpioPin& outPin, hal::GpioPin& inPin, hal::GpioPin& inNPin, Report& report)
+    : ready(false)
+    , reset(resetPin, false)
+    , booted(bootedPin)
+    , out(outPin, false)
+    , in(inPin)
+    , inN(inNPin)
+    , report(report)
+    {}
+
+    void ResetStim();
+    void Run();
+
+private:
+    void Timeout();
+    void Triggered();
+
+private:
+    volatile bool ready;
+    hal::OutputPin reset;
+    hal::InputPin booted;
+    hal::OutputPin out;
+    hal::InputPin in;
+    hal::InputPin inN;
+    Report& report;
+    infra::TimerSingleShot gpioTimeout;
+    infra::TimerSingleShot resetTimer;
+};
+
+void Tester::ResetStim()
+{
+    ready = false;
+    reset.Set(true);
+
+    resetTimer.Start(std::chrono::milliseconds(3), [this]()
+    {
+        assert(booted.Get() == true);
+        booted.EnableInterrupt([this]() {
+            report.State(Report::Condition::Ready);
+            ready = true;
+        }, hal::InterruptTrigger::risingEdge);
+        reset.Set(false);
+    });
+}
+
+void Tester::Run()
+{
+    if (!ready) return;
+
+    if (in.Get() == true)
+    {
+        report.State(Report::Condition::FailedPrecondition);
+        return;
+    }
+    
+    const bool expected = true;
+    const auto expectedEdge = expected ? hal::InterruptTrigger::risingEdge : hal::InterruptTrigger::fallingEdge;
+    gpioTimeout.Start(std::chrono::seconds(1), [this]() { Timeout(); });
+    in.EnableInterrupt([this]() { Triggered(); }, expectedEdge);
+    out.Set(expected);
+}
+
+void Tester::Timeout()
+{
+    in.DisableInterrupt();
+    report.State(Report::Condition::Timeout);
+}
+
+void Tester::Triggered()
+{
+        gpioTimeout.Cancel();
+        if (in.Get() != inN.Get())
+            report.State(Report::Condition::Success);
+        else
+            report.State(Report::Condition::Failed);
+}
+
 int main()
 {
     static main_::StmEventInfrastructure eventInfrastructure;
     static main_::NucleoF767ziUi ui;
-    static services::DebugLed debugLed(ui.ledBlue);    
+
+    // I/O towards Stim
+    static hal::GpioPinStm nResetStimPin(hal::Port::E, 6, hal::Drive::OpenDrain);
+    static services::GpioPinInverted nResetStimPinInverted(nResetStimPin); // false: HiZ, true: Low
+    
+    static hal::GpioPinStm nBootedPin(hal::Port::E, 3, hal::Drive::OpenDrain, hal::Speed::Default, hal::WeakPull::Up);
+    static services::GpioPinInverted nBootedPinInverted(nBootedPin);
 
     static hal::GpioPinStm outPin(hal::Port::F, 7);
     static hal::GpioPinStm inPin(hal::Port::F, 8);
     static hal::GpioPinStm inNPin(hal::Port::F, 9);
-    inPin.Config(hal::PinConfigType::input);
-    inNPin.Config(hal::PinConfigType::input);
 
-    static hal::GpioPinStm nResetStim(hal::Port::E, 6, hal::Drive::OpenDrain);
-    static services::GpioPinInverted resetStim(nResetStim); // false: HiZ, true: Low
-    static hal::GpioPinStm nBooted(hal::Port::E, 3, hal::Drive::OpenDrain, hal::Speed::Default, hal::WeakPull::Up);
-    static services::GpioPinInverted booted(nBooted);
-    booted.Config(hal::PinConfigType::input);
+    // Tester infrastucture
+    static Report report(ui.ledRed, ui.ledGreen);
 
-    outPin.Set(false); // NOTE: is this redundant?
-    resetStim.Set(true);
-    infra::TimerSingleShot resetTimer(std::chrono::milliseconds(3), []()
-        {
-            if (booted.Get() == true) {
-                ui.ledGreen.Set(false);
-                ui.ledRed.Set(true);
-            }
-            else
-            {
-                booted.EnableInterrupt([]() {
-                    ui.ledGreen.Set(true);
-                    ui.ledRed.Set(true);
-                    
-                    ui.userButtonPin.EnableInterrupt([]() {                        
-                        if (inPin.Get() == true)
-                        {
-                            ui.ledGreen.Set(false);
-                            ui.ledRed.Set(true);
-                        }
-                        else
-                        {
-                            static infra::TimerSingleShot gpioTimeout(std::chrono::seconds(1), []() {
-                                inPin.DisableInterrupt();
-                                ui.ledGreen.Set(false);
-                                ui.ledRed.Set(true);
-                            });
+    static Tester tester(nResetStimPinInverted, nBootedPinInverted, outPin, inPin, inNPin, report);
+    tester.ResetStim();
+    static services::DebouncedButton button(ui.userButtonPin, []() { tester.Run(); }); // how to disable until booted?
 
-                            const bool expect = true;
-                            inPin.EnableInterrupt([]() {
-                                gpioTimeout.Cancel();
-                                const auto value = inPin.Get();
-                                const auto nValue = inNPin.Get();
-                                ui.ledGreen.Set(value == expect && nValue == !expect);
-                                ui.ledRed.Set(value != expect || nValue != !expect);
-                            }, hal::InterruptTrigger::risingEdge);
-
-                            outPin.Set(expect);
-                        }
-                    }, hal::InterruptTrigger::fallingEdge);
-                }, hal::InterruptTrigger::risingEdge);
-                resetStim.Set(false);
-            }
-        });
-
+    // Event loop
+    static services::DebugLed debugLed(ui.ledBlue);
     eventInfrastructure.Run();
     __builtin_unreachable();
 }
