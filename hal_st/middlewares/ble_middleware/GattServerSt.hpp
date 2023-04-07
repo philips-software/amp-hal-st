8a24afb36c47d6719fe9c86840f4f88936d256fd
Santos, Gabriel <Gabriel.Santos_1@philips.com>
2023-04-07 08:23:23 +0200

    Merged PR 11085: [ECR] HalSt updated according the GATT interface
    HalSt updated according the GATT interface
    
    (cherry picked from commit c809b11676121f0c12eea92b4b0a2f74755e9829)

--- hal_st/middlewares/ble_middleware/GattServerSt.hpp
+++ hal_st/middlewares/ble_middleware/GattServerSt.hpp
@@ -3,34 +3,34 @@
 
 #include "ble/ble.h"
 #include "hal_st/middlewares/ble_middleware/HciEventObserver.hpp"
-#include "services/ble/Gatt.hpp"
+#include "services/ble/GattServer.hpp"
 
 namespace hal
 {
     class GattServerSt
         : public services::GattServer
-        , public services::GattCharacteristicClientOperations
+        , public services::GattServerCharacteristicOperations
         , public hal::HciEventSink
     {
     public:
         explicit GattServerSt(hal::HciEventSource& hciEventSource);
 
         // Implementation of services::GattServer
-        virtual void AddService(services::GattService& service);
+        virtual void AddService(services::GattServerService& service);
 
         // Implementation of services::GattCharacteristicClientOperations
-        virtual UpdateStatus Update(const services::GattCharacteristicClientOperationsObserver& characteristic, infra::ConstByteRange data) const;
+        virtual UpdateStatus Update(const services::GattServerCharacteristicOperationsObserver& characteristic, infra::ConstByteRange data) const;
 
         // Implementation of hal::HciEventSink
         virtual void HciEvent(hci_event_pckt& event);
 
     protected:
-        virtual void AddCharacteristic(services::GattCharacteristic& characteristic);
+        virtual void AddCharacteristic(services::GattServerCharacteristic& characteristic);
         virtual void HandleGattAttributeModified(aci_gatt_attribute_modified_event_rp0& event);
         virtual void ReportError(tBleStatus status) const;
 
     private:
-        infra::IntrusiveForwardList<services::GattService> services;
+        infra::IntrusiveForwardList<services::GattServerService> services;
     };
 }
 
