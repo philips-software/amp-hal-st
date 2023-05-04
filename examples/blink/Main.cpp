c59fa4845fb4531c438b7115dd174dfcf5ddde01
Richard Peters <richard.peters@philips.com>
2023-05-04 08:45:20 +0200

    Remove project name from modifications.md

--- examples/blink/Main.cpp
+++ examples/blink/Main.cpp
@@ -1,6 +1,6 @@
 #include "hal_st/instantiations/NucleoUi.hpp"
 #include "hal_st/instantiations/StmEventInfrastructure.hpp"
-//#include "hal_st/stm32fxxx/DefaultClockNucleoF767ZI.hpp"
+// #include "hal_st/stm32fxxx/DefaultClockNucleoF767ZI.hpp"
 #include "services/util/DebugLed.hpp"
 
 unsigned int hse_value = 8000000;
@@ -10,7 +10,7 @@ int main()
     HAL_Init();
 
     // Configure your clock here
-    //ConfigureDefaultClockNucleo767ZI();
+    // ConfigureDefaultClockNucleo767ZI();
 
     static main_::StmEventInfrastructure eventInfrastructure;
     static main_::NUCLEO ui;
