a1eaaf53fe8cc032da7aec5a3937f5643c9f6590
Gabriel Santos <118445638+gabrielsantosphilips@users.noreply.github.com>
2023-05-17 16:26:19 +0200

    feat: add data operations to GattClient (#132)
    * [ECR] Data operations added to GattClient
    
    * PR comments processed
    
    * Create private function to handle with notifications and indications

--- hal_st/middlewares/ble_middleware/TracingGattClientSt.hpp
+++ hal_st/middlewares/ble_middleware/TracingGattClientSt.hpp
@@ -17,6 +17,20 @@ namespace hal
         virtual void StartCharacteristicDiscovery(const services::GattService& service) override;
         virtual void StartDescriptorDiscovery(const services::GattService& service) override;
 
+        // Implementation of services::GattClientCharacteristicOperations
+        virtual void Read(const services::GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(const infra::ConstByteRange&)>& onResponse) const override;
+        virtual void Write(const services::GattClientCharacteristicOperationsObserver& characteristic, infra::ConstByteRange data, const infra::Function<void()>& onDone) const override;
+        virtual void WriteWithoutResponse(const services::GattClientCharacteristicOperationsObserver& characteristic, infra::ConstByteRange data) const override;
+
+        virtual void EnableNotification(const services::GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void()>& onDone) const override;
+        virtual void DisableNotification(const services::GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void()>& onDone) const override;
+        virtual void EnableIndication(const services::GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void()>& onDone) const override;
+        virtual void DisableIndication(const services::GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void()>& onDone) const override;
+
+    protected:
+        virtual void HandleGattIndicationEvent(evt_blecore_aci* vendorEvent) override;
+        virtual void HandleGattNotificationEvent(evt_blecore_aci* vendorEvent) override;
+
     private:
         services::Tracer& tracer;
     };
