871eb8c222588198e45c1b6b97dad3adea870d4b
Richard Peters <richard@rpeters.org>
2023-11-09 14:44:59 +0100

    integration_test/tester/Main: Update names of Tester/Tested

--- integration_test/tester/Main.cpp
+++ integration_test/tester/Main.cpp
@@ -8,16 +8,13 @@
 
 // Tester    Tested Function
 // ------    ------ --------
-// CN8-9  -> CN8-9  E5V (NOTE: set JP3 on the Stim to E5V)
+// CN8-9  -> CN8-9  E5V (NOTE: set JP3 on both the Tester and Tested to E5V)
 // CN8-11 -- CN8-11 ground
 // CN8-10 <- CN8-12 ECHO on SESAME
 // CN8-12 -> CN8-10 ECHO on SESAME
-// CN9-20 -> CN8-5  nRst of Stim
+// CN9-20 -> CN8-5  nRst of Tested
 // CN9-24 <- CN9-24 GPIO set by Tester
 // CN9-26 -> CN9-26 Tester GPIO control
-
-// Connect debug session to GDB, bring up with:
-// JLinkGDBServer -select USB -device STM32F767ZI -endian little -if JTAG -speed auto -noir -LocalhostOnly -nologtofile
 
 unsigned int hse_value = 8000000;
 
