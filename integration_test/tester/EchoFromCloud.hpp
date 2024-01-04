27f2f90eedd35c50846136e7e5553ea36d30a212
Richard Peters <richard@rpeters.org>
2024-01-04 11:52:05 +0100

    integration_test/{tester,tested}: Add tracing on ECHO calls

--- integration_test/tester/EchoFromCloud.hpp
+++ integration_test/tester/EchoFromCloud.hpp
@@ -2,8 +2,10 @@
 #define HAL_ST_INTEGRATION_TEST_ECHO_FROM_CLOUD_HPP
 
 #include "generated/echo/Testing.pb.hpp"
-#include "services/util/EchoInstantiation.hpp"
+#include "generated/echo/TracingTesting.pb.hpp"
 #include "hal_st/stm32fxxx/UartStmDuplexDma.hpp"
+#include "services/tracer/TracingEchoInstantiation.hpp"
+#include "services/tracer/GlobalTracer.hpp"
 
 namespace main_
 {
@@ -15,11 +17,17 @@ namespace main_
 
         hal::GpioPinStm hostUartTxPin{ hal::Port::C, 10 };
         hal::GpioPinStm hostUartRxPin{ hal::Port::C, 11 };
-        hal::DmaStm dma;
         hal::UartStmDuplexDma::WithRxBuffer<256> hostUart;
         services::MethodSerializerFactory ::ForServices<testing::Tester, testing::Tested, testing::GpioObserverProxy, testing::GpioTester, testing::GpioTested>::AndProxies<testing::TesterProxy, testing::TestedProxy, testing::GpioObserver, testing::GpioTesterProxy, testing::GpioTestedProxy> serializerFactory;
         hal::BufferedSerialCommunicationOnUnbuffered::WithStorage<256> bufferedHostUart{ hostUart };
-        main_::EchoOnSesame<256> echo{ bufferedHostUart, serializerFactory };
+        services::TracerColoured redTracer{ services::TracerColoured::red, services::GlobalTracer() };
+        main_::TracingEchoOnSesame<256> echo{ bufferedHostUart, serializerFactory, redTracer };
+
+        testing::TesterTracer testerTracer{ echo.echo };
+        testing::TestedTracer testedTracer{ echo.echo };
+        testing::GpioTesterTracer gpioTesterTracer{ echo.echo };
+        testing::GpioTestedTracer gpioTestedTracer{ echo.echo };
+        testing::GpioObserverTracer gpioObserverTracer{ echo.echo };
     };
 }
 
