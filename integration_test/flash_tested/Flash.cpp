75efdbf5eece4f061331ce35232da6d8a09729c3
Richard Peters <richard@rpeters.org>
2024-04-11 16:48:24 +0200

    test: add integration_test.flash_tested

---
+++ integration_test/flash_tested/Flash.cpp
@@ -0,0 +1,145 @@
+#include "integration_test/flash_tested/Flash.hpp"
+#include <utility>
+
+namespace application
+{
+    Flash::Flash(services::Echo& echo, hal::Flash& flash)
+        : flash::Flash(echo)
+        , flashResult(echo)
+        , flash(flash)
+    {}
+
+    void Flash::Read(uint32_t address, uint32_t size)
+    {
+        flash.ReadBuffer(infra::Head(infra::MakeRange(buffer), size), address, [this, size]()
+            {
+                flashResult.RequestSend([this, size]()
+                    {
+                        flashResult.ReadDone(infra::Head(infra::MakeRange(buffer), size));
+                        MethodDone();
+                    });
+            });
+    }
+
+    void Flash::Write(uint32_t address, infra::ConstByteRange contents)
+    {
+        flash.WriteBuffer(contents, address, [this]()
+            {
+                flashResult.RequestSend([this]()
+                    {
+                        flashResult.WriteDone();
+                        MethodDone();
+                    });
+            });
+    }
+
+    void Flash::EraseSectors(uint32_t sector, uint32_t numberOfSectors)
+    {
+        flash.EraseSectors(sector, sector + numberOfSectors, [this]()
+            {
+                flashResult.RequestSend([this]()
+                    {
+                        flashResult.EraseSectorsDone();
+                        MethodDone();
+                    });
+            });
+    }
+
+    FlashProxy::FlashProxy(services::Echo& echo)
+        : flash::FlashResult(echo)
+        , proxy(echo)
+    {}
+
+    void FlashProxy::WriteBuffer(infra::ConstByteRange buffer, uint32_t address, infra::Function<void()> onDone)
+    {
+        writingBuffer = buffer;
+        this->onDone = onDone;
+
+        if (!writingBuffer.empty())
+            proxy.RequestSend([this, address]()
+                {
+                    ++transferBuffers;
+                    auto size = std::min(writingBuffer.size(), std::tuple_size<std::remove_reference_t<decltype(std::declval<flash::WriteRequest>().contents.Storage())>>::value);
+                    proxy.Write(address, infra::Head(writingBuffer, size));
+                    WriteBuffer(infra::DiscardHead(writingBuffer, size), address + size, this->onDone.Clone());
+                });
+    }
+
+    void FlashProxy::ReadBuffer(infra::ByteRange buffer, uint32_t address, infra::Function<void()> onDone)
+    {
+        readingBuffer = buffer;
+        this->onDone = onDone;
+
+        ReadPartialBuffer(address, 0);
+    }
+
+    void FlashProxy::EraseSectors(uint32_t beginIndex, uint32_t endIndex, infra::Function<void()> onDone)
+    {
+        this->onDone = onDone;
+
+        proxy.RequestSend([this, beginIndex, endIndex]()
+            {
+                proxy.EraseSectors(beginIndex, endIndex - beginIndex);
+            });
+    }
+
+    void FlashProxy::ReadDone(infra::ConstByteRange contents)
+    {
+        infra::Copy(contents, infra::Head(readingBuffer, contents.size()));
+        readingBuffer = infra::DiscardHead(readingBuffer, contents.size());
+
+        --transferBuffers;
+        if (transferBuffers == 0 && readingBuffer.empty())
+            onDone();
+    }
+
+    void FlashProxy::WriteDone()
+    {
+        --transferBuffers;
+        if (transferBuffers == 0 && writingBuffer.empty())
+            onDone();
+    }
+
+    void FlashProxy::EraseSectorsDone()
+    {
+        onDone();
+    }
+
+    void FlashProxy::ReadPartialBuffer(uint32_t address, uint32_t start)
+    {
+        if (readingBuffer.size() - start > 0)
+            proxy.RequestSend([this, address, start]()
+                {
+                    ++transferBuffers;
+                    auto size = std::min<uint32_t>(readingBuffer.size() - start, std::tuple_size<std::remove_reference_t<decltype(std::declval<flash::WriteRequest>().contents.Storage())>>::value);
+                    proxy.Read(address + start, size);
+                    ReadPartialBuffer(address, start + size);
+                });
+    }
+
+    FlashHomogeneousProxy::FlashHomogeneousProxy(services::Echo& echo, uint32_t numberOfSectors, uint32_t sizeOfEachSector)
+        : FlashProxy(echo)
+        , numberOfSectors(numberOfSectors)
+        , sizeOfEachSector(sizeOfEachSector)
+    {}
+
+    uint32_t FlashHomogeneousProxy::NumberOfSectors() const
+    {
+        return numberOfSectors;
+    }
+
+    uint32_t FlashHomogeneousProxy::SizeOfSector(uint32_t sectorIndex) const
+    {
+        return sizeOfEachSector;
+    }
+
+    uint32_t FlashHomogeneousProxy::SectorOfAddress(uint32_t address) const
+    {
+        return address / sizeOfEachSector;
+    }
+
+    uint32_t FlashHomogeneousProxy::AddressOfSector(uint32_t sectorIndex) const
+    {
+        return sectorIndex * sizeOfEachSector;
+    }
+}
---
+++ integration_test/flash_tested/Flash.hpp
@@ -0,0 +1,73 @@
+#ifndef HAL_ST_INTEGRATION_TEST_FLASH_HPP
+#define HAL_ST_INTEGRATION_TEST_FLASH_HPP
+
+#include "generated/echo/Flash.pb.hpp"
+#include "hal/interfaces/FlashHomogeneous.hpp"
+#include "infra/util/AutoResetFunction.hpp"
+
+namespace application
+{
+    class Flash
+        : public flash::Flash
+    {
+    public:
+        Flash(services::Echo& echo, hal::Flash& flash);
+
+        // Implementation of flash::Flash
+        void Read(uint32_t address, uint32_t size) override;
+        void Write(uint32_t address, infra::ConstByteRange contents) override;
+        void EraseSectors(uint32_t sector, uint32_t numberOfSectors) override;
+
+    private:
+        flash::FlashResultProxy flashResult;
+        hal::Flash& flash;
+
+        std::array<uint8_t, std::tuple_size<std::remove_reference_t<decltype(std::declval<flash::WriteRequest>().contents.Storage())>>::value> buffer;
+    };
+
+    class FlashProxy
+        : public hal::Flash
+        , private flash::FlashResult
+    {
+    public:
+        FlashProxy(services::Echo& echo);
+
+        // Implementation of hal::Flash
+        void WriteBuffer(infra::ConstByteRange buffer, uint32_t address, infra::Function<void()> onDone) override;
+        void ReadBuffer(infra::ByteRange buffer, uint32_t address, infra::Function<void()> onDone) override;
+        void EraseSectors(uint32_t beginIndex, uint32_t endIndex, infra::Function<void()> onDone) override;
+
+    private:
+        // Implementation of flash::FlashResult
+        void ReadDone(infra::ConstByteRange contents) override;
+        void WriteDone() override;
+        void EraseSectorsDone() override;
+
+        void ReadPartialBuffer(uint32_t address, uint32_t start);
+
+    private:
+        flash::FlashProxy proxy;
+        infra::AutoResetFunction<void()> onDone;
+        infra::ConstByteRange writingBuffer;
+        infra::ByteRange readingBuffer;
+        uint32_t transferBuffers = 0;
+    };
+
+    class FlashHomogeneousProxy
+        : public FlashProxy
+    {
+    public:
+        FlashHomogeneousProxy(services::Echo& echo, uint32_t numberOfSectors, uint32_t sizeOfEachSector);
+
+        uint32_t NumberOfSectors() const override;
+        uint32_t SizeOfSector(uint32_t sectorIndex) const override;
+        uint32_t SectorOfAddress(uint32_t address) const override;
+        uint32_t AddressOfSector(uint32_t sectorIndex) const override;
+
+    private:
+        uint32_t numberOfSectors;
+        uint32_t sizeOfEachSector;
+    };
+}
+
+#endif
