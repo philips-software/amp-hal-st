d199d27a3c2f881817bf1988f75ea61e6459a4b0
Gabriel Santos <gabriel.santos_1@philips.com>
2024-05-28 14:55:39 +0200

    remove non related BLE files

--- hal_st/middlewares/STM32_WPAN/STM32CubeWBA/mac_802_15_4/core/inc/st_mac_802_15_4_mlme_svc.h
+++
@@ -1,9 +0,0 @@
-/** @file st_mac_802_15_4_mlme_svc.h
- *
- * @brief define dedicated MLME SVC Primitives
- */
-
-#ifndef ST_MAC_802_15_4_MLME_SVC_H
-#define ST_MAC_802_15_4_MLME_SVC_H
-
-#endif /* ST_MAC_802_15_4_MLME_SVC_H*/
