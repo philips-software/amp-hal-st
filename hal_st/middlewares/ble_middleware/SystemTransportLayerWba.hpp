f2803c809d12f2944fb4e0655f5fd41f8216e57e
Gabriel Santos <118445638+gabrielsantosphilips@users.noreply.github.com>
2024-07-08 14:21:30 +0200

    feat!: add BLE transport layer for stm32wbaxx (#356)
    add ble transport layer for stm32wbaxx

---
+++ hal_st/middlewares/ble_middleware/SystemTransportLayerWba.hpp
@@ -0,0 +1,21 @@
+#ifndef HAL_ST_SYSTEM_TRANSPORT_LAYER_WBA_HPP
+#define HAL_ST_SYSTEM_TRANSPORT_LAYER_WBA_HPP
+
+#include "hal_st/middlewares/ble_middleware/HciEventObserver.hpp"
+#include "infra/util/InterfaceConnector.hpp"
+
+namespace hal
+{
+    class SystemTransportLayerWba
+        : public infra::InterfaceConnector<SystemTransportLayerWba>
+        , public HciEventSource
+    {
+    public:
+        explicit SystemTransportLayerWba(uint16_t maxAttMtuSize);
+
+        // Implementation of HciEventSource
+        void HciEventHandler(hci_event_pckt& event) override;
+    };
+}
+
+#endif
