4f409d024de94ec7c7534caa1a6f421e39915ae1
Daan Timmer <8293597+daantimmer@users.noreply.github.com>
2024-08-19 12:03:12 +0200

    feat: add support for STM32G4xx DigitalToAnalog peripheral (#375)
    * feat: add support for STM32G4xx DigitalToAnalog peripheral
    
    * Apply suggestions from code review
    
    Co-authored-by: Richard Peters <richard.peters@philips.com>
    
    * chore: process review comments
    
    * chore: fixed DAC build for non G4 devices
    
    ---------
    
    Co-authored-by: Richard Peters <richard.peters@philips.com>

--- hal_st/stm32fxxx/DigitalToAnalogPinStm.cpp
+++ hal_st/stm32fxxx/DigitalToAnalogPinStm.cpp
@@ -1,32 +1,71 @@
 #include "hal_st/stm32fxxx/DigitalToAnalogPinStm.hpp"
-#include DEVICE_HEADER
+#include "generated/stm32fxxx/PeripheralTable.hpp"
+#include "hal_st/stm32fxxx/GpioStm.hpp"
 #include "infra/util/BitLogic.hpp"
+#include "infra/util/EnumCast.hpp"
+#include "infra/util/Optional.hpp"
+#include <algorithm>
+#include <cassert>
+#include <cstdint>
+
+#include DEVICE_HEADER
 
 #ifdef HAS_PERIPHERAL_DAC
 
 namespace hal
 {
+#if defined(DAC_CHIPCONNECT_BOTH)
+    DigitalToAnalogPinImplStm::DigitalToAnalogPinImplStm(Channel channel, hal::DacStm& dac, Internal)
+        : dac{ dac }
+        , channel{ infra::enum_cast(channel) == 1 ? DAC_CHANNEL_1 : DAC_CHANNEL_2 }
+        , internallyConnected{ true }
+    {}
+
+    DigitalToAnalogPinImplStm::DigitalToAnalogPinImplStm(hal::GpioPinStm& pin, hal::DacStm& dac, External)
+        : pin{ infra::inPlace, pin }
+        , dac{ dac }
+        , channel{ pin.DacChannel(dac.index + 1) == 1 ? DAC_CHANNEL_1 : DAC_CHANNEL_2 }
+        , internallyConnected{ false }
+    {}
+
+    DigitalToAnalogPinImplStm::DigitalToAnalogPinImplStm(hal::GpioPinStm& pin, hal::DacStm& dac, Both)
+        : pin{ infra::inPlace, pin }
+        , dac{ dac }
+        , channel{ pin.DacChannel(dac.index + 1) == 1 ? DAC_CHANNEL_1 : DAC_CHANNEL_2 }
+        , internallyConnected{ true }
+
+    {}
+#else
     DigitalToAnalogPinImplStm::DigitalToAnalogPinImplStm(hal::GpioPinStm& pin, hal::DacStm& dac)
-        : pin(pin)
+        : pin{ infra::inPlace, pin }
         , dac(dac)
+        , channel{ pin.DacChannel(dac.index + 1) == 1 ? DAC_CHANNEL_1 : DAC_CHANNEL_2 }
     {
-        channel = pin.DacChannel(dac.index + 1);
     }
+#endif
 
     void DigitalToAnalogPinImplStm::Set(uint16_t value)
     {
-        uint32_t pinChannel = channel == 1 ? DAC_CHANNEL_1 : DAC_CHANNEL_2;
+        DAC_ChannelConfTypeDef sConfig{ 0 };
+        sConfig.DAC_OutputBuffer = pin ? DAC_OUTPUTBUFFER_ENABLE : DAC_OUTPUTBUFFER_DISABLE;
 
-        DAC_ChannelConfTypeDef sConfig;
-        sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
-        sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
-        HAL_StatusTypeDef result = HAL_DAC_ConfigChannel(&dac.handle, &sConfig, pinChannel);
+#if defined(DAC_HIGH_FREQUENCY_INTERFACE_MODE_AUTOMATIC)
+        sConfig.DAC_HighFrequency = DAC_HIGH_FREQUENCY_INTERFACE_MODE_AUTOMATIC;
+#endif
+
+#if defined(DAC_CHIPCONNECT_BOTH)
+        if (pin)
+            sConfig.DAC_ConnectOnChipPeripheral = internallyConnected ? DAC_CHIPCONNECT_BOTH : DAC_CHIPCONNECT_EXTERNAL;
+        else
+            sConfig.DAC_ConnectOnChipPeripheral = DAC_CHIPCONNECT_INTERNAL;
+#endif
+        HAL_StatusTypeDef result = HAL_DAC_ConfigChannel(&dac.handle, &sConfig, channel);
         assert(result == HAL_OK);
 
-        result = HAL_DAC_Start(&dac.handle, pinChannel);
+        result = HAL_DAC_Start(&dac.handle, channel);
         assert(result == HAL_OK);
 
-        dac.Set(pinChannel, DAC_ALIGN_12B_R, std::min(static_cast<uint32_t>(value), (infra::Bit<uint32_t>(12) - 1)));
+        dac.Set(channel, DAC_ALIGN_12B_R, std::min(static_cast<uint32_t>(value), (infra::Bit<uint32_t>(12) - 1)));
     }
 
     DacStm::DacStm(uint8_t oneBasedIndex)
