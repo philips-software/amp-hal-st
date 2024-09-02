75ece182ee8ef45a2d2412e70444f51ec9ff7ba8
Richard Peters <richard.peters@philips.com>
2024-09-02 11:35:49 +0200

    feat: add Cucumber integration tests (#221)
    * Add cucumber submodule
    
    * Add integration_test/test
    
    * .github/workflows/ci.yml: Checkout submodules
    
    * Update amp-embedded-infra-lib
    
    * Update amp-embedded-infra-lib: fix: infra/util/Endian: fix comparison against C++ version
    
    * Update amp-cucumber-cpp-runner-poc
    
    * .github/workflows/ci.yml: Remove Visual Studio 2019 and gcc 7 from tested compilers since they can't handle C++20
    
    * Do some work on integration tests
    
    * Various fixes
    
    * Fix compilation
    
    * Run clang-format
    
    * Update .github/workflows/ci.yml
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update devcontainer
    
    * Exclude gherkin libraries from all target
    
    * feat!: update MessageCommunication to Sesame
    
    * integration_test/test/FixtureEcho: Adapt for Sesame
    
    * CMakeLists: Don't set EXCLUDE_FROM_ALL on gherkin targets because otherwise they fail to install
    
    * integration_test/logic/Gpio: Differentiate between GpioTester and GpioTested's GpioChanged
    
    * integration_test/tested,tester/Main: Add tracing infrastructure
    
    * chore: add integration test board pcb
    
    (cherry picked from commit f20dc01301542bed2334d23807889cddae545de0)
    
    * integration_test/logic/Gpio: Fix order of RequestSend and setting sending
    
    * integration_test: Use UartStmDuplexDma
    
    * CMakeLists: Update amp-embedded-infralib
    
    * integration_test/{tester,tested}/Main: Add missing include
    
    * integration_test/{tester,tested}: Add tracing on ECHO calls
    
    * integration_test/test: Update and fix
    
    * Update amp-cucumber-cpp-runner-poc
    
    * Update amp-cucumber-cpp-runner
    
    * Change amp-cucumber-cpp-runner-poc to amp-cucumber-cpp-runner
    
    * Update amp-cucumber-cpp-runner
    
    * CMakeLists: Rename amp-cucumber-cpp-runner-poc to amp-cucumber-cpp-runner
    
    * launch.json: Set gdbPath to gdb-multiarch
    
    * Apply clang-format
    
    * Update amp-embedded-infralib, reset colour in GlobalTracer
    
    * integration_test: Reset tested before starting a scenario
    
    * Update amp-cucumber-cpp-runner
    
    * integration_test/test: Run tests and EventDispatcher from single thread
    
    * Remove integration_test/test/FixtureSystemChanges
    
    * Move EventDispatcherThreadAware to amp-embedded-infra-lib
    
    * integration_test: Add UART test
    
    * integration_test/test/Hooks: Get serial port to use from command line arguments
    
    * integration_test/test/FixtureEcho: Support Unix serial ports
    
    * Extract FixtureEchoSerial from FixtureEcho
    
    * integration_test/test/FixtureEcho: Refactor FixtureEchoSerial
    
    * integration_test/test/FixtureEcho: Add EchoClientWebSocket
    
    * Add integration_test/test/features/uart_duplex_dma.feature
    
    * Update amp-cucumber-cpp-runner
    
    * Rename integration_test.test to integration_test.runner
    
    * integration_test/tested: Add main for stm32wb55
    
    * integration_test/runner/FixtureEcho: Add EchoClientTcp
    
    * integration_test: Adjust for DMA refactoring
    
    (cherry picked from commit 14689d2b08c4ba89e091c3dd0bdc9645247f100a)
    
    * .github/workflows/ci.yml: fix merge mistake
    
    * CMakeLists: Only build integration_test when building amp-hal-st standalone
    
    * Update amp-embedded-infra-lib
    
    * Remove amp-cucumber-cpp-runner as submodule, use FetchContent instead
    
    * Use different version of amp-cucumber-cpp-runner
    
    * integration_test/tested/CMakeLists: Disable stm32wb55 for now
    
    * chore: resolve some PR comments
    
    * Add EXPECT_TRUE to WaitFor and WaitUntil invocations
    
    * Update amp-devcontainer-cpp and amp-cucumber-cpp-runner
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ron <45816308+rjaegers@users.noreply.github.com>
    Co-authored-by: Arun Magi <Arun.Magi@philips.com>
    Co-authored-by: Rob Ekelmans <rob.ekelmans@philips.com>

--- integration_test/tested/MainStm32f767.cpp
+++ integration_test/tested/MainStm32f767.cpp
@@ -1,8 +1,10 @@
+#include "hal_st/instantiations/NucleoTracerInfrastructure.hpp"
 #include "hal_st/instantiations/NucleoUi.hpp"
 #include "hal_st/instantiations/StmEventInfrastructure.hpp"
 #include "hal_st/stm32fxxx/DefaultClockNucleoF767ZI.hpp"
 #include "integration_test/tested/EchoFromTester.hpp"
 #include "integration_test/tested/Tested.hpp"
+#include "services/tracer/GlobalTracer.hpp"
 #include "services/util/DebugLed.hpp"
 
 unsigned int hse_value = 8000000;
@@ -13,11 +15,16 @@ int main()
     ConfigureDefaultClockNucleo767ZI();
 
     static main_::StmEventInfrastructure eventInfrastructure;
+    static main_::NucleoF767ziTracerInfrastructure tracerInfrastructure;
+    services::SetGlobalTracerInstance(tracerInfrastructure.tracer);
     static main_::Nucleo144Ui ui;
     static services::DebugLed debugLed(ui.ledBlue);
 
-    static main_::EchoFromTester echo;
-    static main_::Tested tested(echo.echo);
+    static hal::DmaStm dma;
+    static main_::EchoFromTester echo(dma);
+    static main_::Tested tested(echo.echo, dma);
+
+    services::GlobalTracer().Trace() << "Starting tested!";
 
     eventInfrastructure.Run();
     __builtin_unreachable();
--- integration_test/tester/EchoFromCloud.hpp
+++ integration_test/tester/EchoFromCloud.hpp
@@ -2,21 +2,40 @@
 #define HAL_ST_INTEGRATION_TEST_ECHO_FROM_CLOUD_HPP
 
 #include "generated/echo/Testing.pb.hpp"
-#include "hal_st/stm32fxxx/UartStmDma.hpp"
-#include "services/util/EchoInstantiation.hpp"
+#include "generated/echo/TracingTesting.pb.hpp"
+#include "hal_st/stm32fxxx/UartStmDuplexDma.hpp"
+#include "services/tracer/GlobalTracer.hpp"
+#include "services/tracer/TracingEchoInstantiation.hpp"
 
 namespace main_
 {
     struct EchoFromCloud
     {
+        EchoFromCloud(hal::DmaStm& dma, services::Tracer& tracer)
+            : transmitStream{ dma, hal::DmaChannelId{ 1, 4, 4 } }
+            , receiveStream{ dma, hal::DmaChannelId{ 1, 2, 4 } }
+            , redTracer{ services::TracerColoured::red, tracer }
+        {}
+
         hal::GpioPinStm hostUartTxPin{ hal::Port::C, 10 };
         hal::GpioPinStm hostUartRxPin{ hal::Port::C, 11 };
-        hal::DmaStm dma;
-        hal::DmaStm::TransmitStream transmitStream{ dma, hal::DmaChannelId{ 1, 4, 4 } };
-        hal::UartStmDma hostUart{ transmitStream, 4, hostUartTxPin, hostUartRxPin };
-        services::MethodSerializerFactory ::ForServices<testing::Tester, testing::Tested, testing::GpioObserverProxy, testing::GpioTester, testing::GpioTested>::AndProxies<testing::TesterProxy, testing::TestedProxy, testing::GpioObserver, testing::GpioTesterProxy, testing::GpioTestedProxy> serializerFactory;
+        hal::DmaStm::TransmitStream transmitStream;
+        hal::DmaStm::ReceiveStream receiveStream;
+        hal::UartStmDuplexDma::WithRxBuffer<256> hostUart{ transmitStream, receiveStream, 4, hostUartTxPin, hostUartRxPin };
+        services::MethodSerializerFactory::ForServices<testing::Tester, testing::Tested, testing::GpioObserverProxy, testing::GpioTester, testing::GpioTested>::AndProxies<testing::TesterProxy, testing::TestedProxy, testing::GpioObserver, testing::GpioTesterProxy, testing::GpioTestedProxy> serializerFactory;
         hal::BufferedSerialCommunicationOnUnbuffered::WithStorage<256> bufferedHostUart{ hostUart };
-        main_::EchoOnSesame<256> echo{ bufferedHostUart, serializerFactory };
+        services::TracerColoured redTracer;
+        main_::TracingEchoOnSesame<256> echo{ bufferedHostUart, serializerFactory, redTracer };
+
+        testing::TesterTracer testerTracer{ echo.echo };
+        testing::TestedTracer testedTracer{ echo.echo };
+        testing::TestedObserverTracer testedObserverTracer{ echo.echo };
+        testing::GpioTesterTracer gpioTesterTracer{ echo.echo };
+        testing::GpioTestedTracer gpioTestedTracer{ echo.echo };
+        testing::GpioObserverTracer gpioObserverTracer{ echo.echo };
+        testing::UartTesterTracer uartTesterTracer{ echo.echo };
+        testing::UartTestedTracer uartTestedTracer{ echo.echo };
+        testing::UartObserverTracer uartObserverTracer{ echo.echo };
     };
 }
 
