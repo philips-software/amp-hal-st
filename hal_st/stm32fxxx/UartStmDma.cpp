e65c694e36fb8d6fea4d6e68700b7dc5543cbfd5
Baris Tanyeri <86656336+BarisTanyeri@users.noreply.github.com>
2024-10-18 15:55:31 +0200

    fix: lpUart peripheral table was not used correctly with UartStmDma (#430)
    * fix: lpUart peripheral table was not used correctly with UartStmDma
    
    * chore: address PR comments

--- hal_st/stm32fxxx/UartStmDma.cpp
+++ hal_st/stm32fxxx/UartStmDma.cpp
@@ -4,63 +4,51 @@
 
 namespace hal
 {
-    namespace
-    {
-        volatile void* TransmitRegister(uint8_t uartIndex)
-        {
-#if defined(USART_TDR_TDR)
-            return &peripheralUart[uartIndex]->TDR;
-#else
-            return &peripheralUart[uartIndex]->DR;
-#endif
-        }
-    }
-
     UartStmDma::UartStmDma(DmaStm::TransmitStream& transmitStream, uint8_t oneBasedIndex, GpioPinStm& uartTx, GpioPinStm& uartRx, const Config& config)
         : UartStm(oneBasedIndex, uartTx, uartRx, config)
-        , transmitDmaChannel{ transmitStream, TransmitRegister(uartIndex), 1, [this]
+        , transmitDmaChannel{ transmitStream, transmitRegister, 1, [this]
             {
                 TransferComplete();
             } }
     {
-        peripheralUart[uartIndex]->CR3 |= USART_CR3_DMAT;
+        uartArray[uartIndex]->CR3 |= USART_CR3_DMAT;
     }
 
     UartStmDma::UartStmDma(DmaStm::TransmitStream& transmitStream, uint8_t oneBasedIndex, GpioPinStm& uartTx, GpioPinStm& uartRx, GpioPinStm& uartRts, GpioPinStm& uartCts, const Config& config)
         : UartStm(oneBasedIndex, uartTx, uartRx, uartRts, uartCts, config)
-        , transmitDmaChannel{ transmitStream, TransmitRegister(uartIndex), 1, [this]
+        , transmitDmaChannel{ transmitStream, transmitRegister, 1, [this]
             {
                 TransferComplete();
             } }
     {
-        peripheralUart[uartIndex]->CR3 |= USART_CR3_DMAT;
+        uartArray[uartIndex]->CR3 |= USART_CR3_DMAT;
     }
 
 #if defined(HAS_PERIPHERAL_LPUART)
     UartStmDma::UartStmDma(DmaStm::TransmitStream& transmitStream, uint8_t oneBasedIndex, GpioPinStm& uartTx, GpioPinStm& uartRx, LpUart lpUart, const Config& config)
         : UartStm(oneBasedIndex, uartTx, uartRx, lpUart, config)
-        , transmitDmaChannel{ transmitStream, TransmitRegister(uartIndex), 1, [this]
+        , transmitDmaChannel{ transmitStream, transmitRegister, 1, [this]
             {
                 TransferComplete();
             } }
     {
-        peripheralUart[uartIndex]->CR3 |= USART_CR3_DMAT;
+        uartArray[uartIndex]->CR3 |= USART_CR3_DMAT;
     }
 
     UartStmDma::UartStmDma(DmaStm::TransmitStream& transmitStream, uint8_t oneBasedIndex, GpioPinStm& uartTx, GpioPinStm& uartRx, GpioPinStm& uartRts, GpioPinStm& uartCts, LpUart lpUart, const Config& config)
         : UartStm(oneBasedIndex, uartTx, uartRx, uartRts, uartCts, lpUart, config)
-        , transmitDmaChannel{ transmitStream, TransmitRegister(uartIndex), 1, [this]
+        , transmitDmaChannel{ transmitStream, transmitRegister, 1, [this]
             {
                 TransferComplete();
             } }
     {
-        peripheralUart[uartIndex]->CR3 |= USART_CR3_DMAT;
+        uartArray[uartIndex]->CR3 |= USART_CR3_DMAT;
     }
 #endif
 
     UartStmDma::~UartStmDma()
     {
-        peripheralUart[uartIndex]->CR1 &= ~(USART_CR1_TE | USART_CR1_RE);
+        uartArray[uartIndex]->CR1 &= ~(USART_CR1_TE | USART_CR1_RE);
         DisableClockUart(uartIndex);
     }
 
@@ -80,9 +68,9 @@ namespace hal
         this->dataReceived = dataReceived;
 
 #if defined(STM32F4) || defined(STM32G4)
-        peripheralUart[uartIndex]->CR1 |= USART_IT_RXNE & USART_IT_MASK;
+        uartArray[uartIndex]->CR1 |= USART_IT_RXNE & USART_IT_MASK;
 #else
-        peripheralUart[uartIndex]->CR1 |= 1 << (USART_IT_RXNE & USART_IT_MASK);
+        uartArray[uartIndex]->CR1 |= 1 << (USART_IT_RXNE & USART_IT_MASK);
 #endif
     }
 
