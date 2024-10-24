0216739168c8e9c132bc0da1f600fa3b4333cd06
EkelmansPh <58972933+EkelmansPh@users.noreply.github.com>
2024-10-24 15:39:36 +0200

    chore: add FlashOnStBootloaderCommunicator (#257)
    * chore: add FlasOnStBootloaderCommunicator
    
    * flas -> flash
    
    * Update amp-embedded-infra-lib
    
    * Apply suggestions from code review
    
    * resolve PR comments
    
    * refactoring classes
    
    * data received but no commandActions in queue
    
    * resolve some PR comments
    
    * move NotifyingVerifyingFunctionMock to amp-embedded-infra-lib
    
    * Update services/st_util/StBootloaderCommunicator.hpp
    
    Co-authored-by: Richard Peters <richard.peters@philips.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Resolve some of the PR comments
    
    * Resolve PR comments
    
    * Update services/st_util/FlashOnStBootloaderCommunicator.cpp
    
    ---------
    
    Co-authored-by: Richard Peters <richard.peters@philips.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ services/st_util/test/TestFlashOnStBootloaderCommunicator.cpp
@@ -0,0 +1,138 @@
+#include "infra/stream/ByteInputStream.hpp"
+#include "infra/stream/ByteOutputStream.hpp"
+#include "infra/util/ByteRange.hpp"
+#include "infra/util/Endian.hpp"
+#include "infra/util/Function.hpp"
+#include "infra/util/test_helper/MockCallback.hpp"
+#include "services/st_util/FlashOnStBootloaderCommunicator.hpp"
+#include "services/st_util/StBootloaderCommunicator.hpp"
+#include "services/st_util/test_doubles/StBootloaderCommunicatorMock.hpp"
+#include "gmock/gmock.h"
+#include <algorithm>
+#include <array>
+#include <cstdint>
+#include <gtest/gtest.h>
+
+class FlashOnStBootloaderCommunicatorTest
+    : public testing::Test
+{
+public:
+    services::StBootloaderCommunicatorMock stBootloaderCommunicator;
+    services::FlashHomogeneousOnStBootloaderCommunicator flashOnStBootloaderCommunicator{ 10, 1, stBootloaderCommunicator };
+};
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, erase_all_sectors)
+{
+    infra::VerifyingFunction<void()> onDone;
+
+    EXPECT_CALL(stBootloaderCommunicator, ExtendedMassErase(services::MassEraseSubcommand::global, testing::_)).WillOnce([](auto, const auto& onDone)
+        {
+            onDone();
+        });
+    flashOnStBootloaderCommunicator.EraseAll(onDone);
+}
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, erase_some_sectors)
+{
+    infra::VerifyingFunction<void()> onDone;
+    uint8_t iterator = 0;
+
+    EXPECT_CALL(stBootloaderCommunicator, ExtendedErase(testing::_, testing::_)).WillRepeatedly([&iterator](auto pages, const auto& onDone)
+        {
+            ASSERT_TRUE(iterator < 5);
+            EXPECT_EQ(pages.front(), infra::BigEndian<uint16_t>{iterator++});
+            onDone();
+        });
+    flashOnStBootloaderCommunicator.EraseSectors(0, 5, onDone);
+}
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, read_small_buffer)
+{
+    std::array<uint8_t, 4> readData = {};
+    std::array<uint8_t, 4> simulatedData = { 0, 1, 2, 3 };
+    infra::ByteInputStream stream(simulatedData);
+
+    EXPECT_CALL(stBootloaderCommunicator, ReadMemory(0x8000000, testing::_, testing::_)).WillOnce([&stream](auto, auto& data, const auto& onDone)
+        {
+            stream >> data;
+            onDone();
+        });
+
+    infra::VerifyingInvoker<void()> onDone([&readData, &simulatedData]()
+        {
+            EXPECT_TRUE(readData == simulatedData);
+        });
+
+    flashOnStBootloaderCommunicator.ReadBuffer(infra::MakeByteRange(readData), 0, onDone);
+}
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, read_big_buffer)
+{
+    std::array<uint8_t, 500> readData = {};
+    std::array<uint8_t, 500> simulatedData = { 0 };
+    std::fill(simulatedData.begin(), simulatedData.end(), 15);
+    infra::ByteInputStream stream(simulatedData);
+
+    EXPECT_CALL(stBootloaderCommunicator, ReadMemory(testing::_, testing::_, testing::_)).Times(2).WillRepeatedly([&stream](auto address, auto& data, const auto& onDone)
+        {
+            static auto relativeAddress = 0;
+            ASSERT_EQ(address, 0x8000000 + relativeAddress);
+            relativeAddress += data.size();
+
+            stream >> data;
+            onDone();
+        });
+
+    infra::VerifyingInvoker<void()> onDone([&readData, &simulatedData]()
+        {
+            EXPECT_TRUE(readData == simulatedData);
+        });
+
+    flashOnStBootloaderCommunicator.ReadBuffer(infra::MakeByteRange(readData), 0, onDone);
+}
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, write_small_buffer)
+{
+    std::array<uint8_t, 4> writeData = { 0, 1, 2, 3 };
+    infra::ByteOutputStream::WithStorage<4> stream;
+
+    EXPECT_CALL(stBootloaderCommunicator, WriteMemory(0x8000000, testing::_, testing::_)).WillOnce([&stream](auto, auto data, const auto& onDone)
+        {
+            stream << data;
+            onDone();
+        });
+
+    infra::VerifyingInvoker<void()> onDone([&writeData, &stream]()
+        {
+            EXPECT_TRUE(writeData == stream.Storage());
+        });
+
+    flashOnStBootloaderCommunicator.WriteBuffer(writeData, 0, onDone);
+}
+
+TEST_F(FlashOnStBootloaderCommunicatorTest, write_big_buffer)
+{
+    std::array<uint8_t, 500> writeData = { 0 };
+    std::fill(writeData.begin(), writeData.end(), 15);
+    infra::ByteOutputStream::WithStorage<500> stream;
+
+    EXPECT_CALL(stBootloaderCommunicator, WriteMemory(testing::_, testing::_, testing::_)).Times(2).WillRepeatedly([&stream](auto address, auto data, const auto& onDone)
+        {
+            stream << data;
+
+            static auto relativeAddress = 0;
+            ASSERT_EQ(address, 0x8000000 + relativeAddress);
+            relativeAddress += data.size();
+
+            onDone();
+        });
+
+    infra::VerifyingInvoker<void()> onDone([&writeData, &stream]()
+        {
+            EXPECT_TRUE(writeData == stream.Storage());
+        });
+
+    flashOnStBootloaderCommunicator.WriteBuffer(writeData, 0, onDone);
+}
+
+// TODO data received but no commandActions in queue
