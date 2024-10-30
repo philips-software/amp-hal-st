84ac60f60b116a8a26afc9842d203bfa0ca95bea
Paul Janssens <81311690+Paul-013040@users.noreply.github.com>
2024-10-30 16:05:37 +0100

    check also not building for F4xx

--- hal_st/stm32fxxx/AnalogToDigitalPinStm.cpp
+++ hal_st/stm32fxxx/AnalogToDigitalPinStm.cpp
@@ -220,7 +220,7 @@ namespace hal
 
 #ifdef IS_ADC_SINGLE_DIFFERENTIAL
         result = HAL_ADCEx_Calibration_Start(&handle, ADC_SINGLE_ENDED);
-#elif !defined(STM32F7xx)
+#elif !defined(STM32F7xx) && !defined(STM32F4xx)
         result = HAL_ADCEx_Calibration_Start(&handle);
 #endif
         assert(result == HAL_OK);
