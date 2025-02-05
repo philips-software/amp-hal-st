a2148474685297527b7aee60e5d4c3ede7d50328
oguzcan oguz <oguz.oguzcan@philips.com>
2025-02-05 11:48:45 +0100

    fix: gap central adv data copy mistake 2  (#502)
    * Fix GapCentral adv data copy mistake
    
    * Fix GapCentral adv data copy mistake 2

--- hal_st/middlewares/ble_middleware/GapCentralSt.cpp
+++ hal_st/middlewares/ble_middleware/GapCentralSt.cpp
@@ -296,7 +296,6 @@ namespace hal
         discoveredDevice.eventType = ToAdvertisingEventType(advertisingReport.Event_Type);
         discoveredDevice.addressType = ToAdvertisingAddressType(advertisingReport.Address_Type);
         std::copy(advertisementData, advertisementData + advertisingReport.Length_Data, discoveredDevice.data.begin());
-        std::copy(discoveredDevice.data.begin(), discoveredDevice.data.begin() + advertisingReport.Length_Data, advertisementData);
         discoveredDevice.rssi = static_cast<int8_t>(*const_cast<uint8_t*>(advertisementData + advertisingReport.Length_Data));
 
         infra::Subject<services::GapCentralObserver>::NotifyObservers([&discoveredDevice](auto& observer)
