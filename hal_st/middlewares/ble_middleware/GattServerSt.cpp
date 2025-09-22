0dbcab953e260cc76b4d8c3645a42924f564b106
Brian van der Beek <brian.vander.beek@philips.com>
2025-09-22 09:11:21 +0000

    hack to add report reference descriptor

--- hal_st/middlewares/ble_middleware/GattServerSt.cpp
+++ hal_st/middlewares/ble_middleware/GattServerSt.cpp
@@ -1,4 +1,5 @@
 #include "hal_st/middlewares/ble_middleware/GattServerSt.hpp"
+#include "services/tracer/GlobalTracer.hpp"
 
 namespace
 {
@@ -43,15 +44,66 @@ namespace hal
     void GattServerSt::AddService(services::GattServerService& service)
     {
         constexpr uint8_t gattPrimaryService = 0x01;
+        // HACK: Add extra attributes for HID service descriptors
+        uint8_t attributeCount = service.GetAttributeCount();
+        if (service.Type().Is<services::AttAttribute::Uuid16>() &&
+            service.Type().Get<services::AttAttribute::Uuid16>() == 0x1812) // HID Service UUID
+        {
+            attributeCount += 1; // Add 1 for the Report Reference descriptor (only for Report characteristic)
+            services::GlobalTracer().Trace() << "GattServerSt::AddService: Adding HID Service with " << static_cast<uint32_t>(attributeCount) << " attributes";
+        }
 
         auto result = aci_gatt_add_service(UuidToType(service.Type()), ConvertUuid<Service_UUID_t>(service.Type()),
-            gattPrimaryService, service.GetAttributeCount(), &service.Handle());
+            gattPrimaryService, attributeCount, &service.Handle());
 
         if (result != BLE_STATUS_SUCCESS)
             ReportError(result);
 
+        services::GlobalTracer().Trace() << "GattServerSt::AddService: Adding characteristics...";
         for (auto& characteristic : service.Characteristics())
+        {
+            auto charUuid = characteristic.Type().Get<services::AttAttribute::Uuid16>();
+
+            services::GlobalTracer().Trace() << "GattServerSt::AddService: Adding characteristic UUID=0x" << infra::hex << charUuid;
             AddCharacteristic(characteristic);
+
+            if (charUuid == 0x2A4D) // HID Report characteristic
+            {
+                // HACK: Add Report Reference descriptors immediately after adding 0x2A4D descriptor. <--- shows 0x2908 immediately after 0x2A4D in nRF connect, but now keys do not work in android.  Keypress does not work in windows
+                services::GlobalTracer().Trace() << "GattServerSt::AddService: Found HID Report characteristic (0x2A4D), adding Report Reference descriptor";
+                constexpr services::AttAttribute::Uuid16 reportRefDescUuid{ 0x2908 };
+                constexpr uint8_t reportRefData[] = { 0x01, 0x01 }; // Report ID 1, Input Report
+                AddDescriptor(characteristic, reportRefDescUuid, infra::MakeConstByteRange(reportRefData));
+            }
+        }
+
+        // HACK: Add Report Reference descriptors AFTER all characteristics and CCCDs are created   <---- works on android, but shows 0x2908 as last entry under 0x2a4a (Hid information) in nRF connect. Windows connects, but keypress does not work
+        // This ensures we don't interfere with the automatic CCCD creation for notify characteristics
+        // if (service.Type().Is<services::AttAttribute::Uuid16>() &&
+        //     service.Type().Get<services::AttAttribute::Uuid16>() == 0x1812) // HID Service UUID
+        // {
+        //     services::GlobalTracer().Trace() << "GattServerSt::AddService: Looking for HID Report characteristic to add descriptor...";
+        //     // Find the specific Report characteristic (0x2A4D) and add descriptor only to it
+        //     for (auto& characteristic : service.Characteristics())
+        //     {
+        //         if (characteristic.Type().Is<services::AttAttribute::Uuid16>())
+        //         {
+        //             auto charUuid = characteristic.Type().Get<services::AttAttribute::Uuid16>();
+        //             services::GlobalTracer().Trace() << "GattServerSt::AddService: Checking characteristic UUID=0x" << infra::hex << charUuid;
+
+        //             // Add Report Reference descriptor to Report characteristic (0x2A4D) ONLY
+        //             // Boot Protocol characteristics should NOT have Report Reference descriptors per HID spec
+        //             if (charUuid == 0x2A4D) // HID Report characteristic
+        //             {
+        //                 services::GlobalTracer().Trace() << "GattServerSt::AddService: Found HID Report characteristic (0x2A4D), adding Report Reference descriptor";
+        //                 constexpr services::AttAttribute::Uuid16 reportRefDescUuid{ 0x2908 };
+        //                 constexpr uint8_t reportRefData[] = { 0x01, 0x01 }; // Report ID 1, Input Report (fix: correct report ID)
+        //                 AddDescriptor(characteristic, reportRefDescUuid, infra::MakeConstByteRange(reportRefData));
+        //                 break; // Only add to the first (and should be only) Report characteristic
+        //             }
+        //         }
+        //     }
+        // }
 
         services.push_front(service);
     }
@@ -78,6 +130,11 @@ namespace hal
 
     void GattServerSt::AddDescriptor(const services::GattServerCharacteristicOperationsObserver& characteristic, const services::AttAttribute::Uuid& uuid, infra::ConstByteRange data)
     {
+        services::GlobalTracer().Trace() << "GattServerSt::AddDescriptor: Adding descriptor UUID=0x"
+                                         << infra::hex << (uuid.Is<services::AttAttribute::Uuid16>() ? uuid.Get<services::AttAttribute::Uuid16>() : 0)
+                                         << ", size=" << data.size()
+                                         << ", data=0x" << infra::AsHex(data);
+
         constexpr uint8_t encryptionKeySize = 0x10;
         constexpr uint8_t attributePermissions = ATTR_PERMISSION_NONE;
         constexpr uint8_t attributeAccess = ATTR_ACCESS_READ_ONLY;
@@ -101,7 +158,16 @@ namespace hal
             &descriptorHandle);
 
         if (result != BLE_STATUS_SUCCESS)
+        {
+            services::GlobalTracer().Trace() << "GattServerSt::AddDescriptor: Failed with result=0x"
+                                             << infra::hex << static_cast<uint32_t>(result);
             ReportError(result);
+        }
+        else
+        {
+            services::GlobalTracer().Trace() << "GattServerSt::AddDescriptor: Success, handle=0x"
+                                             << infra::hex << descriptorHandle;
+        }
     }
 
     void GattServerSt::HciEvent(hci_event_pckt& event)
