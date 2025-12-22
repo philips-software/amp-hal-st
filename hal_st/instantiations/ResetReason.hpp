697e23fbf1d1dc5730875f87970861445cc12cce
Hein Wessels <hein.wessels@philips.com>
2025-12-22 09:19:41 +0000

    feat: add reset reason

---
+++ hal_st/instantiations/ResetReason.hpp
@@ -0,0 +1,34 @@
+#ifndef HAL_ST_INSTANTIATIONS_RESETREASON_HPP
+#define HAL_ST_INSTANTIATIONS_RESETREASON_HPP
+
+#include "infra/util/BoundedString.hpp"
+#include DEVICE_HEADER
+
+namespace hal
+{
+    // Note: This function clears the reset flags after reading them
+    infra::BoundedConstString GetResetReason()
+    {
+        const char* resetReason;
+
+        if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST))
+            resetReason = "LowPower";
+        else if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST))
+            resetReason = "Window WatchDog";
+        else if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST))
+            resetReason = "Independent WatchDog";
+        else if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST))
+            resetReason = "Software";
+        else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST))
+            resetReason = "Reset Pin";
+        else if (__HAL_RCC_GET_FLAG(RCC_FLAG_BORRST))
+            resetReason = "BrownOut(BOR)";
+        else
+            resetReason = "Unknown";
+
+        __HAL_RCC_CLEAR_RESET_FLAGS();
+        return resetReason;
+    }
+}
+
+#endif // HAL_ST_INSTANTIATIONS_RESETREASON_HPP
