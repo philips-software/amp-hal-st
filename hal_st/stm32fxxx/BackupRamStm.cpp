7e6c7acdb3f688fd3869382f6c753af7d8bca524
Alexander Paersch <alexander.paersch@uxitra.de>
2026-03-11 12:59:20 +0000

    Fix backup ram for stm32h5xx

--- hal_st/stm32fxxx/BackupRamStm.cpp
+++ hal_st/stm32fxxx/BackupRamStm.cpp
@@ -7,8 +7,11 @@ namespace hal
     {
 #if defined(STM32G0)
         return infra::MakeRange(&TAMP->BKP0R, &TAMP->BKP4R + 1);
-#elif defined(STM32G4) || defined(STM32WBA) || defined(STM32H5)
+#elif defined(STM32G4) || defined(STM32WBA)
         return infra::MakeRange(&TAMP->BKP0R, &TAMP->BKP15R + 1);
+#elif defined(STM32H5)
+        auto* base = reinterpret_cast<volatile uint32_t*>(BKPSRAM_BASE);
+        return infra::MakeRange(base, base + BKPSRAM_SIZE / sizeof(uint32_t));
 #else
         return infra::MakeRange(&peripheralRtc[0]->BKP0R, &peripheralRtc[0]->BKP19R + 1);
 #endif
