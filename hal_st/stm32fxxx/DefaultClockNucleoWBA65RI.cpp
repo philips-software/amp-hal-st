71c362fb02d705de334af0d0d2fd2d60f8f2d3b7
Cassio Romanin de Lazaro <cassio.romanindelazaro@philips.com>
2026-08-18 09:52:37 +0000

    tests lower clock

--- hal_st/stm32fxxx/DefaultClockNucleoWBA65RI.cpp
+++ hal_st/stm32fxxx/DefaultClockNucleoWBA65RI.cpp
@@ -2,54 +2,56 @@
 #include "hal_st/stm32fxxx/DefaultClockNucleoWBA65RI.hpp"
 
 /* The system Clock is configured as follows:
- *            System Clock source            = PLL (HSE)
- *            SYSCLK(Hz)                     = 100000000
- *            HCLK(Hz)                       = 100000000
+ *            System Clock source            = HSE
+ *            SYSCLK(Hz)                     = 32000000
+ *            HCLK(Hz)                       = 32000000
  *            AHB Prescaler                  = 1
  *            APB1 Prescaler                 = 1
  *            APB2 Prescaler                 = 1
  *            APB7 Prescaler                 = 1
  *            HSE Frequency(Hz)              = 32000000
- *            PLL_M                          = 4
- *            PLL_N                          = 25
- *            PLL_P                          = 2
- *            PLL_Q                          = 1
- *            PLL_R                          = 2
- *            Flash Latency(WS)              = 4
+ *            PLL                            = OFF
+ *            Flash Latency(WS)              = 0
  */
 void ConfigureDefaultClockNucleoWBA65RI()
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
     RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-    __HAL_RCC_PWR_CLK_ENABLE();
+    HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY);
 
     HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /* Activate PLL with HSE as source */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    /** Configure LSE Drive Capability
+     */
+    // HAL_PWR_EnableBkUpAccess();
+    __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_MEDIUMLOW);
+
+    /* Enable HSE, PLL disabled */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_HSE | RCC_OSCILLATORTYPE_LSE;
     RCC_OscInitStruct.HSEState = RCC_HSE_ON;
     RCC_OscInitStruct.HSEDiv = RCC_HSE_DIV1;
-    RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL1.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL1.PLLM = 4;
-    RCC_OscInitStruct.PLL1.PLLN = 25; /* VCO = HSE/M * N = 32 / 4 * 25 = 200 MHz */
-    RCC_OscInitStruct.PLL1.PLLR = 2;  /* PLLSYS = 200 MHz / 2 = 100 MHz */
-    RCC_OscInitStruct.PLL1.PLLP = 2;
-    RCC_OscInitStruct.PLL1.PLLQ = 2;
-    RCC_OscInitStruct.PLL1.PLLFractional = 0;
+    RCC_OscInitStruct.LSEState = RCC_LSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL1.PLLState = RCC_PLL_NONE;
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    /* Select PLL as system clock source and configure the HCLK, PCLK1, PCLK2 and PCLK7
+    /* Select HSE as system clock source and configure the HCLK, PCLK1, PCLK2 and PCLK7
        clocks dividers */
-    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 |
-                                   RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK7 | RCC_CLOCKTYPE_HCLK5);
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 | RCC_CLOCKTYPE_PCLK7 | RCC_CLOCKTYPE_HCLK5;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
     RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB7CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.AHB5_PLL1_CLKDivider = RCC_SYSCLK_PLL1_DIV4;
+    RCC_ClkInitStruct.AHB5_PLL1_CLKDivider = RCC_SYSCLK_PLL1_DIV1;
     RCC_ClkInitStruct.AHB5_HSEHSI_CLKDivider = RCC_SYSCLK_HSEHSI_DIV1;
     HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
+
+    /* Select SysTick source clock */
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK_DIV8);
+
+    /* Re-Initialize Tick with new clock source */
+    HAL_InitTick(TICK_INT_PRIORITY);
 }
--- hal_st/stm32fxxx/LowPowerStrategyStm.cpp
+++ hal_st/stm32fxxx/LowPowerStrategyStm.cpp
@@ -6,12 +6,14 @@ namespace hal
     void LowPowerStrategyStm::RequestExecution()
     {
         __DSB();
+        __ISB();
         __SEV();
     }
 
     void LowPowerStrategyStm::Idle(const infra::EventDispatcherWorker& eventDispatcher)
     {
         __DSB();
+        __ISB();
         __WFE();
     }
 }
