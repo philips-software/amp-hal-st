0767caedc5e8adc1abb54f27ab45b7ac18f88e54
Hein Wessels <hein.wessels@philips.com>
2026-09-07 07:35:54 +0000

    Rename

--- hal_st/middlewares/ble_middleware/BondStorageSt.hpp
+++ hal_st/middlewares/ble_middleware/BondStorageSt.hpp
@@ -11,7 +11,7 @@ extern "C"
 namespace hal
 {
     class BondStorageSt
-        : public services::BondStorageAbsolute
+        : public services::AuthoritativeBondStorage
     {
     public:
         explicit BondStorageSt(uint32_t maxNumberOfBonds);
